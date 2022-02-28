287fa9ef796246ec80db4e286a2ff7791ae2d4e3
Vaibhav Singh <singhvaibhav1710@gmail.com>
2022-02-28 22:10:38 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,71 @@
+import { isLabelWithInternallyDisabledControl } from '@testing-library/user-event/dist/utils';
+import React, { useState } from 'react'
+ 
+export default function About() {
+
+    const [myStyle,setMyStyle] =  useState({
+        color:'black',
+        backgroundColor : 'white' 
+    });
+
+    const [btnstate , setbtnstate] = useState("Dark Mode");
+
+   const togglestyle = () => {
+        if(myStyle.color === 'white'){
+            setMyStyle({
+                color: 'black',
+                backgroundColor:'white'
+            });
+            setbtnstate("Dark Mode");
+        }else{
+         
+            setMyStyle({
+                color: 'white',
+                backgroundColor:'black',
+                border:'1px solid white'
+            });
+            setbtnstate("Light Mode");
+        }
+    }
+
+  return (
+ <div className='container' style ={myStyle} >
+     <h1>About Us</h1>
+      <div className="accordion accordion-flush" style ={myStyle} id="accordionFlushExample">
+  <div className="accordion-item" style ={myStyle} >
+    <h2 className="accordion-header" id="flush-headingOne">
+      <button className="accordion-button collapsed" style ={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="flush-collapseOne" className="accordion-collapse collapse" style ={myStyle} aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the first item's accordion body.</div>
+    </div>
+  </div>
+  <div className="accordion-item" style ={myStyle} >
+    <h2 className="accordion-header" id="flush-headingTwo">
+      <button className="accordion-button collapsed" style ={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+        Accordion Item #2
+      </button>
+    </h2>
+    <div id="flush-collapseTwo" style ={myStyle} className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+      <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+    </div>
+  </div>
+  <div className="accordion-item" style ={myStyle}>
+    <h2 className="accordion-header" id="flush-headingThree">
+      <button className="accordion-button collapsed" style ={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+        Accordion Item #3
+      </button>
+      </h2>
+      <div id="flush-collapseThree" style ={myStyle} className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+      <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+      </div>
+     </div>
+     </div>
+     <div className="container mx-2">
+     <button onClick={togglestyle} type="button" className="btn btn-primary">{btnstate}</button>
+     </div> 
+ </div>
+  )
+}
